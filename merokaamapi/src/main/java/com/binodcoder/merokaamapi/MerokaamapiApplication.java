4813f9370d3851297c6604b4876a67e151c0a9d8
binodcoder <binodcoder@gmail.com>
2024-08-10 17:41:59 +0100

    API project for merokaam initialized

---
+++ merokaamapi/src/main/java/com/binodcoder/merokaamapi/MerokaamapiApplication.java
@@ -0,0 +1,13 @@
+package com.binodcoder.merokaamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerokaamapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerokaamapiApplication.class, args);
+	}
+
+}
