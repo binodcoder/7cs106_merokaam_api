b286d256a124eff6f3c2cd9b2f0b0b4e633aab66
binodcoder <binodcoder@gmail.com>
2024-08-10 18:52:18 +0100

    database configuration added in properties file and controller updated

--- merokaamapi/src/main/java/com/binodcoder/merokaamapi/controller/JobSeekerProfileController.java
+++ merokaamapi/src/main/java/com/binodcoder/merokaamapi/controller/JobSeekerProfileController.java
@@ -1,3 +1,28 @@
 package com.binodcoder.merokaamapi.controller;
+
+import com.binodcoder.merokaamapi.entity.JobSeekerProfile;
+import com.binodcoder.merokaamapi.service.JobSeekerProfileService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api")
 public class JobSeekerProfileController {
+
+    private final JobSeekerProfileService jobSeekerProfileService;
+
+    public JobSeekerProfileController(JobSeekerProfileService jobSeekerProfileService) {
+        this.jobSeekerProfileService = jobSeekerProfileService;
+    }
+
+    @GetMapping("/profile_only")
+    public ResponseEntity<JobSeekerProfile> getOne() {
+        Optional<JobSeekerProfile> optionalProfile = jobSeekerProfileService.getOne(5);
+        return new ResponseEntity<>(optionalProfile.get(), HttpStatus.OK);
+    }
 }
